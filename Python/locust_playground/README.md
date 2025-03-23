ec21784d4118c0dfedd26ea8e97965f4e09fca62
Chet Sheng <hfsg@novonordisk.com>
2025-03-23 16:44:42 +0000

    add locust example

---
+++ Python/locust_playground/README.md
@@ -0,0 +1,7 @@
+```bash
+uvsh
+
+locust
+# specify intended web portal
+locust  --web-port 8886
+```
---
+++ Python/locust_playground/locustfile.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import random
+import time
+
+from locust import User, tag, task, constant, events
+
+
+class ToyStressTest(User):
+    wait_time = constant(0)
+    idx=0
+
+    @tag("simple-query")
+    @task
+    def similarity_search(self):
+        start_time = time.time()
+        
+        try:
+            if self.idx > 3_000_000:
+                10/0
+            
+            res = 1+1
+            self.idx += 1
+            total_time = int((time.time() - start_time) * 1000)
+            events.request.fire(
+                request_type="Custom",
+                name="vector_search",
+                response_time=total_time,
+                response_length=len(str(res)),
+            )
+            # print(f"\nQuery: {raw_query}\nResult: {res}\n=========================")
+
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            print("\n===========================================================================")
+            print(f"Exception: {e}")
+            events.request.fire(
+            # events.user_error.fire(
+                request_type="Custom",
+                name="vector_search",
+                response_time=total_time,
+                response_length=0,
+                exception=str(e),
+            )
